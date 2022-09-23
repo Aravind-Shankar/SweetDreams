d26311252abc18a3b77c38d69142e011a989ae8b
Lyon77 <rjl7799@gmail.com>
2022-09-22 20:26:20 -0400

    Added Start and Pause Menu

---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerController : MonoBehaviour
+{
+    public float speed = 10.0f;
+
+    private Rigidbody rb;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody>();
+    }
+
+    void Update()
+    {
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        float moveUp = 0.0f;
+        if (Input.GetKey(KeyCode.Space) && rb.velocity.y == 0.0f)
+            moveUp = 40.0f;
+
+        Vector3 movement = new Vector3(moveHorizontal, moveUp, moveVertical);
+
+        rb.AddForce(movement * speed);
+    }
+}
