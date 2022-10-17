caa61911baf1b9cf1a9be23cc0beb216f49ebe18
Lyon77 <rjl7799@gmail.com>
2022-10-17 12:56:38 -0400

    Added Temp Player and Player Camera Movement

---
+++ Assets/Scripts/Input/PlayerLocomotion.cs
@@ -0,0 +1,90 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerLocomotion : MonoBehaviour
+{
+    InputManager inputManager;
+    public LayerMask groundLayer;
+
+    Vector3 moveDirection;
+    Transform cameraObject;
+    Rigidbody playerRigidbody;
+
+    public bool isSprinting;
+
+    public float walkingSpeed = 1.5f;
+    public float runningSpeed = 5;
+    public float sprintingSpeed = 7;
+    public float rotationSpeed = 15;
+
+    public float rayCastHeightOffset = 0.5f;
+
+    private void Awake() {
+        inputManager = GetComponent<InputManager>();
+        playerRigidbody = GetComponent<Rigidbody>();
+        cameraObject = Camera.main.transform;
+    }
+
+    public void HandleAllMovement() {
+        HandleMovement();
+        HandleRotation();
+        HandleStairsSlopes();
+    }
+
+    private void HandleMovement() {
+        moveDirection = cameraObject.forward * inputManager.verticalInput;
+        moveDirection += cameraObject.right * inputManager.horizontalInput;
+        moveDirection.Normalize();
+        moveDirection.y = 0;
+
+        if (isSprinting)
+            moveDirection *= sprintingSpeed;
+        else {
+            if (inputManager.moveAmount >= 0.5f)
+                moveDirection *= runningSpeed;
+            else
+                moveDirection *= walkingSpeed;
+        }
+
+        
+
+        Vector3 movementVelocity = moveDirection;
+        playerRigidbody.velocity = movementVelocity;
+    }
+
+    private void HandleRotation() {
+        Vector3 targetDirection = Vector3.zero;
+
+        targetDirection = cameraObject.forward * inputManager.verticalInput;
+        targetDirection += cameraObject.right * inputManager.horizontalInput;
+        targetDirection.Normalize();
+        targetDirection.y = 0;
+
+        if (targetDirection == Vector3.zero)
+            targetDirection = transform.forward;
+
+        Quaternion targetRotation = Quaternion.LookRotation(targetDirection);
+        Quaternion playerRotation = Quaternion.Slerp(transform.rotation, targetRotation, rotationSpeed * Time.deltaTime);
+
+        transform.rotation = playerRotation;
+    }
+
+    private void HandleStairsSlopes() {
+        RaycastHit hit;
+        Vector3 rayCastOrigin = transform.position;
+        Vector3 targetPosition = transform.position;
+        rayCastOrigin.y += rayCastHeightOffset;
+
+        if (Physics.SphereCast(rayCastOrigin, 0.2f, -Vector3.up, out hit, groundLayer)) {
+            Vector3 rayCastHitPoint = hit.point;
+            targetPosition.y = rayCastHitPoint.y;
+        }
+
+        if (inputManager.moveAmount > 0) {
+            transform.position = Vector3.Lerp(transform.position, targetPosition, Time.deltaTime / 0.1f);
+        } else {
+            transform.position = targetPosition;
+        }
+    }
+}
--- Assets/Scripts/Utility/PauseMenuToggle.cs
+++ Assets/Scripts/Utility/PauseMenuToggle.cs
@@ -6,8 +6,11 @@ using UnityEngine;
 public class PauseMenuToggle : MonoBehaviour
 {
     private CanvasGroup canvasGroup;
+    public CameraManager cameraManager;
+
     void Awake() {
         canvasGroup = GetComponent<CanvasGroup>();
+        cameraManager = FindObjectOfType<CameraManager>();
         if (canvasGroup == null) {
             Debug.LogError("No Canvas Group added!");
         }
@@ -21,12 +24,14 @@ public class PauseMenuToggle : MonoBehaviour
                 canvasGroup.interactable = false;
                 canvasGroup.blocksRaycasts = false;
                 canvasGroup.alpha = 0f;
+                cameraManager.pauseGame = false;
 
                 Time.timeScale = 1f;
             } else {
                 canvasGroup.interactable = true;
                 canvasGroup.blocksRaycasts = true;
                 canvasGroup.alpha = 1f;
+                cameraManager.pauseGame = true;
 
                 Time.timeScale = 0f;
             }
