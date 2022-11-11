9d9af0702a130d452740275f49d082f95f159e9b
anarayanan68 <anarayanan68@gatech.edu>
2022-11-11 09:32:17 -0500

    quitter now works in editor too

--- Assets/Scripts/Utility/GameQuitter.cs
+++ Assets/Scripts/Utility/GameQuitter.cs
@@ -1,10 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor;
 using UnityEngine;
 
 public class GameQuitter : MonoBehaviour
 {
     public void Quit() {
+    #if UNITY_EDITOR
+        EditorApplication.isPlaying = false;
+    #else
         Application.Quit();
+    #endif
     }
 }
