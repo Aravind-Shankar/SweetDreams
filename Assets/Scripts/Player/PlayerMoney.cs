d569ca21901edc9b053cd785efa8a89caf7f7d7f
anarayanan68 <anarayanan68@gatech.edu>
2022-11-12 18:41:03 -0500

    money display UI added

--- Assets/Scripts/Player/PlayerMoney.cs
+++ Assets/Scripts/Player/PlayerMoney.cs
@@ -1,16 +1,18 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class PlayerMoney : MonoBehaviour
 {
     public int startingMoney = 100;
+    public Text moneyText;
 
     int _money;
     public int Money
     {
         get { return _money; }
-        set { _money = Mathf.Max(value, 0); }
+        set { _money = Mathf.Max(value, 0); moneyText.text = _money.ToString(); }
     }
 
     private void Start()
