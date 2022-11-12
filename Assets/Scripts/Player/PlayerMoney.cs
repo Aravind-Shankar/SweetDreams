18aabfb40a7f732945365e7a10be9af58a065a37
anarayanan68 <anarayanan68@gatech.edu>
2022-11-12 18:59:27 -0500

    money display now uses TMP

--- Assets/Scripts/Player/PlayerMoney.cs
+++ Assets/Scripts/Player/PlayerMoney.cs
@@ -1,12 +1,13 @@
 using System.Collections;
 using System.Collections.Generic;
+using TMPro;
 using UnityEngine;
-using UnityEngine.UI;
 
 public class PlayerMoney : MonoBehaviour
 {
+    public TextMeshProUGUI moneyText;
+
     public int startingMoney = 100;
-    public Text moneyText;
 
     int _money;
     public int Money
