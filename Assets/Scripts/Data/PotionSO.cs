3d33e250eace46ed2c9c429647f732226081b276
anarayanan68 <anarayanan68@gatech.edu>
2022-11-13 20:46:48 -0500

    potion panel now has button to show recipe in event log | ingredient list becomes pair<ingr_id,count> list in SO

--- Assets/Scripts/Data/PotionSO.cs
+++ Assets/Scripts/Data/PotionSO.cs
@@ -13,11 +13,18 @@ public class PotionSO : ScriptableObject
 }
 
 [System.Serializable]
+public struct IngredientIDCountPair
+{
+    public int id;
+    public int count;
+}
+
+[System.Serializable]
 public class Potion
 {
     public string name;
     public int id;
-    public int[] ingredientIds;
+    public IngredientIDCountPair[] ingredientComposition;
     public int sellingPrice;
     public Sprite icon;
     public ItemType itemType;
--- Assets/Scripts/Inventory/PotionPanel.cs
+++ Assets/Scripts/Inventory/PotionPanel.cs
@@ -11,8 +11,10 @@ public class PotionPanel : MonoBehaviour
     public TextMeshProUGUI potionNameText;
     public CanvasGroup priceCanvasGroup; 
     public TextMeshProUGUI potionPriceText;
+    public IngredientSO ingredientSO;
 
     private CanvasGroup _overallCanvasGroup;
+    private Potion _potion;
 
     private void Awake()
     {
@@ -23,15 +25,40 @@ public class PotionPanel : MonoBehaviour
     public void Clear()
     {
         _overallCanvasGroup.alpha = 0;
+        _overallCanvasGroup.interactable = false;
+        _overallCanvasGroup.blocksRaycasts = false;
+        _potion = null;
     }
 
     public void SetPotion(Potion potion)
     {
         _overallCanvasGroup.alpha = 1;
+        _overallCanvasGroup.interactable = true;
+        _overallCanvasGroup.blocksRaycasts = true;
+        _potion = potion;
 
         potionIcon.sprite = potion.icon;
         potionNameText.text = potion.name;
         priceCanvasGroup.alpha = 1;
         potionPriceText.text = potion.sellingPrice.ToString();
+    }
+
+    public void ShowRecipe()
+    {
+        if (_potion == null)
+        {
+            Debug.LogError("Trying to show recipe of null potion!");
+            return;
+        }
+
+        Color logColor = Color.blue, headerColor = Color.magenta;
+        EventLog.Instance.AddLog($"*****************", headerColor);
+        foreach (var pair in _potion.ingredientComposition)
+        {
+            EventLog.Instance.AddLog($"{ingredientSO.ingredients[pair.id].name} : {pair.count}x", logColor);
+        }
+        EventLog.Instance.AddLog("Dream Liquid (from Dream Machine)", logColor);
+        EventLog.Instance.AddLog($"{_potion.name} recipe:", headerColor);
+        EventLog.Instance.AddLog($"*****************", headerColor);
     }
 }
